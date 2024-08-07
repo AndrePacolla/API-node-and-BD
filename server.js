881eb0b506d8001918022f1bae010aee77840a92
AndrePacolla <andreluizzpacolla@outlook.com>
2024-08-07 19:34:07 -0300

    adc status para cada tipo de rotas

--- server.js
+++ server.js
@@ -7,18 +7,17 @@ app.use(express.json());
 const users = [];
 
 //Rota de Criação.
-app.post('/usuarios',(req, res) =>{  /*(req vem do front) , pega dados que foi criado pelo front e da um push no array*/
+app.post('/usuarios',(req, res) =>{  /*(req vem do front) ,SERVE PARA VER O QUE ESTA CHEGANDO PARA MIM LA DO FRONT.vou pegar dados que foi criado pelo front e da um push no array , 201 cód. criado*/
 
     users.push(req.body)
-
-
+    res.status(201).json(req.json) 
 
 })
 
 //Rota de consumir , exibir dados
 
 app.get('/usuarios',(req, res) =>{
-    res.json(users)
+    res.status(200).json(users)
 
 });
 
